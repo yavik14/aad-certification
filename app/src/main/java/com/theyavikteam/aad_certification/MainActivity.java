50f4b22e105de0bdb4bd489b050cc585231ffb1a
javier.rodriguez <javier.rodriguez@s-dos.es>
2019-01-28 18:25:33 +0100

    create android project

---
+++ app/src/main/java/com/theyavikteam/aad_certification/MainActivity.java
@@ -0,0 +1,13 @@
+package com.theyavikteam.aad_certification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
